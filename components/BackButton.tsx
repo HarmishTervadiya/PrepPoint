79552b0bd2720fc57c8716e9a5dce4df2e2cfa73
HarmishTervadiya <harmishtervadiya@gmail.com>
2025-06-11 19:37:54 +0530

    Changed the navigation for back button

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,39 +1,46 @@
-import {Alert, BackHandler, Image, StyleSheet, TouchableOpacity, View} from 'react-native';
+import {
+  Alert,
+  BackHandler,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 import {useTheme} from '@react-navigation/native';
 import {CustomTheme} from '@/types/customTheme';
 import BackArrow from '@/assets/images/icons/back-arrow.png';
-import { router, useRouter, useSegments } from 'expo-router';
+import {router, useRouter, useSegments} from 'expo-router';
 
 const BackButton = () => {
   const {customColors} = useTheme() as CustomTheme;
   const segments = useSegments();
-  
+
   const navigateBack = () => {
-    if(router.canGoBack()){
-      const currentRoute = segments[segments.length - 1];
-      if(currentRoute === 'userLogin') {
-        router.replace('/(main)'); 
-      } else if(currentRoute === 'userDashboard'){
-        router.replace('/(main)'); 
-      } else {
-        router.back();
-      }
+    const currentRoute = segments[segments.length - 1];
+    if (currentRoute === 'userLogin') {
+      router.replace('/(main)');
+    } else if (currentRoute === 'userDashboard') {
+      router.replace('/(main)');
     } else {
-      Alert.alert("Exit",'Are you sure you want to go back?', [
-        {
-          text: "cancel",
-          style: 'cancel'
-        },
-        {
-          text: "yes",
-          onPress: () => BackHandler.exitApp(),
-          style: 'default'
-        },
-      ])
+      if (router.canGoBack()) {
+        router.back();
+      } else {
+        Alert.alert('Exit', 'Are you sure you want to go back?', [
+          {
+            text: 'cancel',
+            style: 'cancel',
+          },
+          {
+            text: 'yes',
+            onPress: () => BackHandler.exitApp(),
+            style: 'default',
+          },
+        ]);
+      }
     }
-  }
- 
+  };
+
   return (
     <TouchableOpacity
       style={[styles.card, {backgroundColor: customColors.backBtn}]}
